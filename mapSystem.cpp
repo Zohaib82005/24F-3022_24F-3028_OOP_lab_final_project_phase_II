c59993d2249799ec097b996efe10b60a3f635c17
Muhammad Zohaib <mzohaibfakhar786@gmail.com>
2025-05-14 22:27:01 +0500

    Update mapSystem.cpp

--- mapSystem.cpp
+++ mapSystem.cpp
@@ -12,7 +12,8 @@ MapSystem::MapSystem(int w, int h) : width(w), height(h) {
     }
 }
 
-void MapSystem::placeKingdom(const std::string& name, int x, int y) {
+void MapSystem::placeKingdom(const  string& name, int x, int y) {
+
     grid[x][y] = name[0];
 }
 
@@ -27,35 +28,38 @@ void MapSystem::moveKingdom(const std::string& name, const std::string& directio
     else if (direction == "left" && pos.x > 0) pos.x--;
     else if (direction == "right" && pos.x < width - 1) pos.x++;
     else std::cout << "Invalid move or out of bounds.\n";
+    placeKingdom(name, pos.x, pos.y);
 }
+
+
 
 void MapSystem::displayMap() const {
 
-    std::cout << "\n--- Kingdom Map ---\n";
+     cout << "\n--- Kingdom Map ---\n";
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            std::cout << grid[y][x] << " ";
+             cout << grid[y][x] << " ";
         }
-        std::cout << "\n";
+         cout << "\n";
     }
-    std::cout << "\nLegend:\n";
+     cout << "\nLegend:\n";
     for (const auto& entry : kingdomPositions) {
-        std::cout << toupper(entry.first[0]) << " = " << entry.first
+         cout << toupper(entry.first[0]) << " = " << entry.first
             << " at (" << entry.second.x << "," << entry.second.y << ")\n";
     }
 
 }
 
-Position MapSystem::getPosition(const std::string& name) const {
+Position MapSystem::getPosition(const  string& name) const {
     auto it = kingdomPositions.find(name);
     if (it != kingdomPositions.end()) return it->second;
-    return { -1, -1 }; // Invalid position
+    return { -1, -1 }; 
 }
 
-void MapSystem::buildStructure(const std::string& name, std::string structure) {
+void MapSystem::buildStructure(const  string& name,  string structure) {
     grid[rand() % 10][rand() % 10] = structure[0];
 }
 
-bool MapSystem::hasStructure(const std::string& name) const {
+bool MapSystem::hasStructure(const  string& name) const {
     return structures.count(name) > 0;
 }
